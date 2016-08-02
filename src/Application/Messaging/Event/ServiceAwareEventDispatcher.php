16e4ae52cc193d30e744b7281afc1e367affe5b4
John Nickell <jnickell@rategenius.com>
2016-08-02 15:18:23 -0500

    event messaging

---
+++ src/Application/Messaging/Event/ServiceAwareEventDispatcher.php
@@ -0,0 +1,197 @@
+<?php declare(strict_types = 1);
+
+namespace Novuso\Common\Application\Messaging\Event;
+
+use Novuso\Common\Application\Service\Container;
+use Novuso\Common\Domain\Messaging\Event\Event;
+use Novuso\Common\Domain\Messaging\Event\EventSubscriber;
+use Novuso\System\Utility\ClassName;
+use Novuso\System\Utility\Test;
+
+/**
+ * ServiceAwareEventDispatcher dispatches events to subscriber services
+ *
+ * @copyright Copyright (c) 2016, Novuso. <http://novuso.com>
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ * @author    John Nickell <email@johnnickell.com>
+ */
+class ServiceAwareEventDispatcher extends SimpleEventDispatcher
+{
+    /**
+     * Service container
+     *
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * Services
+     *
+     * @var array
+     */
+    protected $services = [];
+
+    /**
+     * Service IDs
+     *
+     * @var array
+     */
+    protected $serviceIds = [];
+
+    /**
+     * Constructs ServiceAwareEventDispatcher
+     *
+     * @param Container $container The service container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Registers a subscriber service to handle events
+     *
+     * The subscriber class must implement:
+     * Novuso\Common\Domain\Messaging\Event\EventSubscriber
+     *
+     * @param string $className The subscriber class name
+     * @param string $serviceId The subscriber service ID
+     *
+     * @return void
+     */
+    public function registerService(string $className, string $serviceId)
+    {
+        assert(
+            Test::implementsInterface($className, EventSubscriber::class),
+            sprintf('Invalid subscriber class: %s', $className)
+        );
+        /** @var EventSubscriber $className The subscriber class name */
+        foreach ($className::eventRegistration() as $eventType => $params) {
+            if (is_string($params)) {
+                $this->addHandlerService($eventType, $serviceId, $params);
+            } elseif (is_string($params[0])) {
+                $priority = isset($params[1]) ? (int) $params[1] : 0;
+                $this->addHandlerService($eventType, $serviceId, $params[0], $priority);
+            } else {
+                foreach ($params as $handler) {
+                    $priority = isset($handler[1]) ? (int) $handler[1] : 0;
+                    $this->addHandlerService($eventType, $serviceId, $handler[0], $priority);
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds a handler service for a specific event
+     *
+     * @param string $eventType The event type
+     * @param string $serviceId The handler service ID
+     * @param string $method    The name of the method to invoke
+     * @param int    $priority  Higher priority handlers are called first
+     *
+     * @return void
+     */
+    public function addHandlerService(string $eventType, string $serviceId, string $method, int $priority = 0)
+    {
+        if (!isset($this->serviceIds[$eventType])) {
+            $this->serviceIds[$eventType] = [];
+        }
+
+        $this->serviceIds[$eventType][] = [$serviceId, $method, $priority];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch(Event $event)
+    {
+        $this->lazyLoad(ClassName::underscore($event));
+
+        parent::dispatch($event);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHandlers(string $eventType = null): array
+    {
+        if ($eventType === null) {
+            foreach (array_keys($this->serviceIds) as $type) {
+                $this->lazyLoad($type);
+            }
+        } else {
+            $this->lazyLoad($eventType);
+        }
+
+        return parent::getHandlers($eventType);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasHandlers(string $eventType = null): bool
+    {
+        if ($eventType === null) {
+            return (bool) count($this->serviceIds) || (bool) count($this->handlers);
+        }
+
+        if (isset($this->serviceIds[$eventType])) {
+            return true;
+        }
+
+        return parent::hasHandlers($eventType);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeHandler(string $eventType, callable $handler)
+    {
+        $this->lazyLoad($eventType);
+
+        if (isset($this->serviceIds[$eventType])) {
+            foreach ($this->serviceIds[$eventType] as $i => $args) {
+                list($serviceId, $method) = $args;
+                $key = $serviceId.'.'.$method;
+                if (isset($this->services[$eventType][$key])
+                    && $handler === [$this->services[$eventType][$key], $method]) {
+                    unset($this->services[$eventType][$key]);
+                    if (empty($this->services[$eventType])) {
+                        unset($this->services[$eventType]);
+                    }
+                    unset($this->serviceIds[$eventType][$i]);
+                    if (empty($this->serviceIds[$eventType])) {
+                        unset($this->serviceIds[$eventType]);
+                    }
+                }
+            }
+        }
+
+        parent::removeHandler($eventType, $handler);
+    }
+
+    /**
+     * Lazy loads event handlers from the service container
+     *
+     * @param string $eventType The event type
+     *
+     * @return void
+     */
+    protected function lazyLoad(string $eventType)
+    {
+        if (isset($this->serviceIds[$eventType])) {
+            foreach ($this->serviceIds[$eventType] as $args) {
+                list($serviceId, $method, $priority) = $args;
+                $service = $this->container->get($serviceId);
+                $key = $serviceId.'.'.$method;
+                if (!isset($this->services[$eventType][$key])) {
+                    $this->addHandler($eventType, [$service, $method], $priority);
+                } elseif ($service !== $this->services[$eventType][$key]) {
+                    parent::removeHandler($eventType, [$this->services[$eventType][$key], $method]);
+                    $this->addHandler($eventType, [$service, $method], $priority);
+                }
+                $this->services[$eventType][$key] = $service;
+            }
+        }
+    }
+}
--- src/Application/Messaging/Event/SimpleEventDispatcher.php
+++ src/Application/Messaging/Event/SimpleEventDispatcher.php
@@ -4,7 +4,9 @@ namespace Novuso\Common\Application\Messaging\Event;
 
 use Novuso\Common\Domain\Messaging\Event\Event;
 use Novuso\Common\Domain\Messaging\Event\EventDispatcher;
+use Novuso\Common\Domain\Messaging\Event\EventMessage;
 use Novuso\Common\Domain\Messaging\Event\EventSubscriber;
+use Novuso\System\Utility\ClassName;
 
 /**
  * SimpleEventDispatcher is a synchronous event dispatcher
@@ -16,10 +18,34 @@ use Novuso\Common\Domain\Messaging\Event\EventSubscriber;
 class SimpleEventDispatcher implements EventDispatcher
 {
     /**
+     * Event handlers
+     *
+     * @var array
+     */
+    protected $handlers = [];
+
+    /**
+     * Sorted handlers
+     *
+     * @var array
+     */
+    protected $sorted = [];
+
+    /**
      * {@inheritdoc}
      */
     public function dispatch(Event $event)
     {
+        $message = EventMessage::create($event);
+        $eventType = ClassName::underscore($event);
+
+        foreach ($this->getHandlers($eventType) as $handler) {
+            call_user_func($handler, $message);
+        }
+
+        foreach ($this->getHandlers(EventSubscriber::ALL_EVENTS) as $handler) {
+            call_user_func($handler, $message);
+        }
     }
 
     /**
@@ -27,6 +53,19 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function register(EventSubscriber $subscriber)
     {
+        foreach ($subscriber->eventRegistration() as $eventType => $params) {
+            if (is_string($params)) {
+                $this->addHandler($eventType, [$subscriber, $params]);
+            } elseif (is_string($params[0])) {
+                $priority = isset($params[1]) ? (int) $params[1] : 0;
+                $this->addHandler($eventType, [$subscriber, $params[0]], $priority);
+            } else {
+                foreach ($params as $handler) {
+                    $priority = isset($handler[1]) ? (int) $handler[1] : 0;
+                    $this->addHandler($eventType, [$subscriber, $handler[0]], $priority);
+                }
+            }
+        }
     }
 
     /**
@@ -34,6 +73,16 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function unregister(EventSubscriber $subscriber)
     {
+        foreach ($subscriber->eventRegistration() as $eventType => $params) {
+            if (is_array($params) && is_array($params[0])) {
+                foreach ($params as $handler) {
+                    $this->removeHandler($eventType, [$subscriber, $handler[0]]);
+                }
+            } else {
+                $handler = is_string($params) ? $params : $params[0];
+                $this->removeHandler($eventType, [$subscriber, $handler]);
+            }
+        }
     }
 
     /**
@@ -41,6 +90,16 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function addHandler(string $eventType, callable $handler, int $priority = 0)
     {
+        if (!isset($this->handlers[$eventType])) {
+            $this->handlers[$eventType] = [];
+        }
+
+        if (!isset($this->handlers[$eventType][$priority])) {
+            $this->handlers[$eventType][$priority] = [];
+        }
+
+        $this->handlers[$eventType][$priority][] = $handler;
+        unset($this->sorted[$eventType]);
     }
 
     /**
@@ -48,6 +107,25 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function getHandlers(string $eventType = null): array
     {
+        if ($eventType !== null) {
+            if (!isset($this->handlers[$eventType])) {
+                return [];
+            }
+
+            if (!isset($this->sorted[$eventType])) {
+                $this->sortHandlers($eventType);
+            }
+
+            return $this->sorted[$eventType];
+        }
+
+        foreach (array_keys($this->handlers) as $eventType) {
+            if (!isset($this->sorted[$eventType])) {
+                $this->sortHandlers($eventType);
+            }
+        }
+
+        return array_filter($this->sorted);
     }
 
     /**
@@ -55,6 +133,7 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function hasHandlers(string $eventType = null): bool
     {
+        return (bool) count($this->getHandlers($eventType));
     }
 
     /**
@@ -62,5 +141,41 @@ class SimpleEventDispatcher implements EventDispatcher
      */
     public function removeHandler(string $eventType, callable $handler)
     {
+        if (!isset($this->handlers[$eventType])) {
+            return;
+        }
+
+        foreach ($this->handlers[$eventType] as $priority => $handlers) {
+            $key = array_search($handler, $handlers, true);
+            if ($key !== false) {
+                unset($this->handlers[$eventType][$priority][$key]);
+                unset($this->sorted[$eventType]);
+                if (empty($this->handlers[$eventType][$priority])) {
+                    unset($this->handlers[$eventType][$priority]);
+                }
+                if (empty($this->handlers[$eventType])) {
+                    unset($this->handlers[$eventType]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Sorts event handlers by priority
+     *
+     * @param string $eventType The event type
+     *
+     * @return void
+     */
+    protected function sortHandlers(string $eventType)
+    {
+        $this->sorted[$eventType] = [];
+        if (isset($this->handlers[$eventType])) {
+            krsort($this->handlers[$eventType]);
+            $this->sorted[$eventType] = call_user_func_array(
+                'array_merge',
+                $this->handlers[$eventType]
+            );
+        }
     }
 }
