d39ad031bb55ce11745c9b3e8a126f2c9546f9f6
John Nickell <jnickell@rategenius.com>
2016-08-02 10:48:26 -0500

    query router

---
+++ src/Application/Messaging/Query/Routing/InMemoryQueryMap.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types = 1);
+
+namespace Novuso\Common\Application\Messaging\Query\Routing;
+
+use Novuso\Common\Domain\Messaging\Query\Query;
+use Novuso\Common\Domain\Messaging\Query\QueryHandler;
+use Novuso\System\Exception\DomainException;
+use Novuso\System\Exception\LookupException;
+use Novuso\System\Type\Type;
+use Novuso\System\Utility\Test;
+
+/**
+ * InMemoryQueryMap is a query class to handler instance map
+ *
+ * @copyright Copyright (c) 2016, Novuso. <http://novuso.com>
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ * @author    John Nickell <email@johnnickell.com>
+ */
+class InMemoryQueryMap
+{
+    /**
+     * Query handlers
+     *
+     * @var array
+     */
+    protected $handlers = [];
+
+    /**
+     * Registers query handlers
+     *
+     * The query to handler map must follow this format:
+     * [
+     *     SomeQuery::class => $someHandlerInstance
+     * ]
+     *
+     * @param array $queryToHandlerMap A map of class names to handlers
+     *
+     * @return void
+     *
+     * @throws DomainException When a query class is not valid
+     */
+    public function registerHandlers(array $queryToHandlerMap)
+    {
+        foreach ($queryToHandlerMap as $queryClass => $handler) {
+            $this->registerHandler($queryClass, $handler);
+        }
+    }
+
+    /**
+     * Registers a query handler
+     *
+     * @param string       $queryClass The full query class name
+     * @param QueryHandler $handler    The query handler
+     *
+     * @return void
+     *
+     * @throws DomainException When the query class is not valid
+     */
+    public function registerHandler(string $queryClass, QueryHandler $handler)
+    {
+        if (!Test::implementsInterface($queryClass, Query::class)) {
+            $message = sprintf('Invalid query class: %s', $queryClass);
+            throw new DomainException($message);
+        }
+
+        $type = Type::create($queryClass)->toString();
+
+        $this->handlers[$type] = $handler;
+    }
+
+    /**
+     * Retrieves handler by query class name
+     *
+     * @param string $queryClass The full query class name
+     *
+     * @return QueryHandler
+     *
+     * @throws LookupException When a handler is not registered
+     */
+    public function getHandler(string $queryClass): QueryHandler
+    {
+        $type = Type::create($queryClass)->toString();
+
+        if (!isset($this->handlers[$type])) {
+            $message = sprintf('Handler not defined for query: %s', $queryClass);
+            throw new LookupException($message);
+        }
+
+        return $this->handlers[$type];
+    }
+
+    /**
+     * Checks if a handler is defined for a query
+     *
+     * @param string $queryClass The full query class name
+     *
+     * @return bool
+     */
+    public function hasHandler(string $queryClass): bool
+    {
+        $type = Type::create($queryClass)->toString();
+
+        return isset($this->handlers[$type]);
+    }
+}
---
+++ src/Application/Messaging/Query/Routing/InMemoryQueryRouter.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace Novuso\Common\Application\Messaging\Query\Routing;
+
+use Novuso\Common\Domain\Messaging\Query\Query;
+use Novuso\Common\Domain\Messaging\Query\QueryHandler;
+
+/**
+ * InMemoryQueryRouter matches queries from an in-memory map
+ *
+ * @copyright Copyright (c) 2016, Novuso. <http://novuso.com>
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ * @author    John Nickell <email@johnnickell.com>
+ */
+class InMemoryQueryRouter implements QueryRouter
+{
+    /**
+     * Query map
+     *
+     * @var InMemoryQueryMap
+     */
+    protected $queryMap;
+
+    /**
+     * Constructs InMemoryQueryRouter
+     *
+     * @param InMemoryQueryMap $queryMap The query map
+     */
+    public function __construct(InMemoryQueryMap $queryMap)
+    {
+        $this->queryMap = $queryMap;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match(Query $query): QueryHandler
+    {
+        return $this->queryMap->getHandler(get_class($query));
+    }
+}
