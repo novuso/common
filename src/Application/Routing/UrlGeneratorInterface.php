8767eef8885434949ef866681d3fc4e29001035c
John Nickell <email@johnnickell.com>
2017-07-27 15:23:32 -0500

    updated url generator and moved concrete template engines

--- src/Application/Routing/UrlGeneratorInterface.php
+++ src/Application/Routing/UrlGeneratorInterface.php
@@ -19,11 +19,12 @@ interface UrlGeneratorInterface
      *
      * @param string $name       The route name
      * @param array  $parameters An array of route parameters
+     * @param array  $query      An array of query string parameters
      * @param bool   $absolute   Whether or not the URL should be absolute
      *
      * @return Url
      *
      * @throws UrlGenerationException When an error occurs
      */
-    public function generate(string $name, array $parameters = [], bool $absolute = false): Url;
+    public function generate(string $name, array $parameters = [], array $query = [], bool $absolute = false): Url;
 }
